0295d72b55f1fb777fb746ef0ea59f06284d99b9
no quiero <no@gmail.com>
2026-04-22 14:24:13 -0500

    CRUD 22/04/26

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { AnimalComponent } from './components/animal-component/animal-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AnimalComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('webzoo');
-  alias="val";
+  alias = "val";
 }
